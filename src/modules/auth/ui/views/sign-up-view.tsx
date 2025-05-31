e434fb24d3ff1ae5c9be199ba44db1b40b1ebe66
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-05-31 18:01:28 +0800

    create dashboard sidebar

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -231,7 +231,7 @@ const SignUpView = () => {
               </div>
             </form>
           </Form>
-          <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+          <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center">
             <Image
               src="/logo.svg"
               alt="logo"
