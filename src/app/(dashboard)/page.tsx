5f11b16f367ad6ef5b483670cf0939538382b7cb
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-06-01 08:47:42 +0800

    TRPC Setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,9 +1,12 @@
 import { auth } from "@/lib/auth";
 import HomeView from "@/modules/home/ui/views/home-view";
+import { caller } from "@/trpc/server";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 const page = async() => {
+  const data=await caller.hello({text:"Agus From TRPC Server"});
+
   const session=await auth.api.getSession({
     headers:await headers(),
   });
@@ -11,6 +14,7 @@ const page = async() => {
   if(!session){
     redirect("/sign-in");
   }
+  return <p>{data.greeting}</p>
   return (
     <HomeView/>
   )
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,31 +1,16 @@
 "use client"
-import { Button } from '@/components/ui/button'
-import { authClient } from '@/lib/auth-client'
-import { useRouter } from 'next/navigation'
+
+import { useTRPC } from "@/trpc/client"
+import { useQuery } from "@tanstack/react-query"
+
 
 const HomeView = () => {
-  const router=useRouter()
-  const { data: session } = authClient.useSession();
+  const trpc=useTRPC()
+  const {data}=useQuery(trpc.hello.queryOptions({text:"Agus From TRPC Client"}))
 
-  if(!session) {
-    return (
-      <div className="flex flex-col gap-y-10">
-        <div className="flex items-center justify-center">
-          <h1 className="text-3xl font-bold">Loading...</h1>
-        </div>
-      </div>
-    )
-  }
   return (
     <div className="flex flex-col gap-y-10">
-      <div className="flex items-center justify-center">
-        <h1 className="text-3xl font-bold">Hello {session?.user?.name}</h1>
-      </div>
-      <Button onClick={() => authClient.signOut({fetchOptions:{
-        onSuccess:()=>{
-          router.push('/sign-in')
-        }
-      }})}>Sign out</Button>
+      {data?.greeting}
     </div>
   )
 }
