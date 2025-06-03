cda83034a12f5ddd3c125ecd431513cf56ff1f8e
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-06-03 13:59:36 +0800

    add dev-tool prettier

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,91 +1,86 @@
-import { z } from "zod";
-import { db } from "@/db";
-import { agents } from "@/db/schema";
-import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { agentInsertSchema } from "../schema";
-import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
+import { z } from 'zod';
+import { db } from '@/db';
+import { agents } from '@/db/schema';
+import { createTRPCRouter, protectedProcedure } from '@/trpc/init';
+import { agentInsertSchema } from '../schema';
+import { and, count, desc, eq, getTableColumns, ilike, sql } from 'drizzle-orm';
 import {
-  DEFAULT_PAGE,
-  DEFAULT_PAGE_SIZE,
-  MAX_PAGE_SIZE,
-  MIN_PAGE_SIZE,
-} from "../../../../constants";
+    DEFAULT_PAGE,
+    DEFAULT_PAGE_SIZE,
+    MAX_PAGE_SIZE,
+    MIN_PAGE_SIZE,
+} from '../../../../constants';
 
 export const agentsRouter = createTRPCRouter({
-  getOne: protectedProcedure
-    .input(z.object({ id: z.string() }))
-    .query(async ({ input }) => {
-      const [existingAgent] = await db
-        .select({
-          //TODO - change this to actual count
-          meetingCount: sql<number>`6`,
-          ...getTableColumns(agents),
-        })
-        .from(agents)
-        .where(eq(agents.id, input.id));
-      return existingAgent;
+    getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
+        const [existingAgent] = await db
+            .select({
+                //TODO - change this to actual count
+                meetingCount: sql<number>`6`,
+                ...getTableColumns(agents),
+            })
+            .from(agents)
+            .where(eq(agents.id, input.id));
+        return existingAgent;
     }),
 
-  getMany: protectedProcedure
-    .input(
-      z.object({
-        page: z.number().default(DEFAULT_PAGE),
-        pageSize: z
-          .number()
-          .min(MIN_PAGE_SIZE)
-          .max(MAX_PAGE_SIZE)
-          .default(DEFAULT_PAGE_SIZE),
-        search: z.string().nullish(),
-      })
-    )
-    .query(async ({ ctx, input }) => {
-      const { search, page, pageSize } = input;
-      const data = await db
-        .select({
-          //TODO - change this to actual count
-          meetingCount: sql<number>`6`,
-          ...getTableColumns(agents),
-        })
-        .from(agents)
-        .where(
-          and(
-            eq(agents.userId, ctx.auth.user.id),
-            search ? ilike(agents.name, `%${search}%`) : undefined
-          )
+    getMany: protectedProcedure
+        .input(
+            z.object({
+                page: z.number().default(DEFAULT_PAGE),
+                pageSize: z
+                    .number()
+                    .min(MIN_PAGE_SIZE)
+                    .max(MAX_PAGE_SIZE)
+                    .default(DEFAULT_PAGE_SIZE),
+                search: z.string().nullish(),
+            })
         )
-        .orderBy(desc(agents.createdAt), desc(agents.id))
-        .limit(pageSize)
-        .offset((page - 1) * pageSize);
+        .query(async ({ ctx, input }) => {
+            const { search, page, pageSize } = input;
+            const data = await db
+                .select({
+                    //TODO - change this to actual count
+                    meetingCount: sql<number>`6`,
+                    ...getTableColumns(agents),
+                })
+                .from(agents)
+                .where(
+                    and(
+                        eq(agents.userId, ctx.auth.user.id),
+                        search ? ilike(agents.name, `%${search}%`) : undefined
+                    )
+                )
+                .orderBy(desc(agents.createdAt), desc(agents.id))
+                .limit(pageSize)
+                .offset((page - 1) * pageSize);
 
-      const [total] = await db
-        .select({ count: count() })
-        .from(agents)
-        .where(
-          and(
-            eq(agents.userId, ctx.auth.user.id),
-            search ? ilike(agents.name, `%${search}%`) : undefined
-          )
-        );
+            const [total] = await db
+                .select({ count: count() })
+                .from(agents)
+                .where(
+                    and(
+                        eq(agents.userId, ctx.auth.user.id),
+                        search ? ilike(agents.name, `%${search}%`) : undefined
+                    )
+                );
 
-      const totalPages = Math.ceil(total.count / pageSize); 
+            const totalPages = Math.ceil(total.count / pageSize);
 
-      return {
-        items: data,
-        total: total.count,
-        totalPages,
-      }
-      
-    }),
-  create: protectedProcedure
-    .input(agentInsertSchema)
-    .mutation(async ({ input, ctx }) => {
-      const [createdAgent] = await db
-        .insert(agents)
-        .values({
-          ...input,
-          userId: ctx.auth.user.id,
-        })
-        .returning();
-      return createdAgent;
+            return {
+                items: data,
+                total: total.count,
+                totalPages,
+            };
+        }),
+    create: protectedProcedure.input(agentInsertSchema).mutation(async ({ input, ctx }) => {
+        const [createdAgent] = await db
+            .insert(agents)
+            .values({
+                ...input,
+                userId: ctx.auth.user.id,
+            })
+            .returning();
+        return createdAgent;
     }),
 });
--- src/modules/agents/ui/components/agent-list-header.tsx
+++ src/modules/agents/ui/components/agent-list-header.tsx
@@ -1,50 +1,46 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { PlusIcon, XCircleIcon } from "lucide-react";
-import React, { useState } from "react";
-import NewAgentDialog from "./new-agent-dialog";
-import { useAgentsFilters } from "../../hooks/use-agents-filter";
-import { AgentsSearchFilter } from "./agent-search-filter";
-import { DEFAULT_PAGE } from "../../../../../constants";
+'use client';
+import { Button } from '@/components/ui/button';
+import { PlusIcon, XCircleIcon } from 'lucide-react';
+import React, { useState } from 'react';
+import NewAgentDialog from './new-agent-dialog';
+import { useAgentsFilters } from '../../hooks/use-agents-filter';
+import { AgentsSearchFilter } from './agent-search-filter';
+import { DEFAULT_PAGE } from '../../../../../constants';
 
 const AgentListHeader = () => {
-  const [filters, setFilters] = useAgentsFilters();
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const [filters, setFilters] = useAgentsFilters();
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-  const isAnygFilterModified = !!filters.search;
-  const onClearFilters = () => {
-    setFilters({
-      search: "",
-      page: DEFAULT_PAGE,
-    });
-  };
-  return (
-    <>
-      <NewAgentDialog open={isDialogOpen} onOpenChange={setIsDialogOpen} />
-      <div className="py-4 px-4 md:px-8 flex flex-col gap-y-4">
-        <div className="flex items-center justify-between">
-          <h5 className="font-medium text-xl">My Agents</h5>
-          <Button onClick={() => setIsDialogOpen(true)}>
-            <PlusIcon />
-            New Agent
-          </Button>
-        </div>
-        <div className="flex items-center gap-x-2 p-1">
-          <AgentsSearchFilter />
-          {isAnygFilterModified && (
-            <Button 
-              variant={"outline"}
-              size={"sm"}
-              onClick={onClearFilters}
-            > 
-              <XCircleIcon />
-              Clear
-            </Button>
-          )}
-        </div>
-      </div>
-    </>
-  );
+    const isAnygFilterModified = !!filters.search;
+    const onClearFilters = () => {
+        setFilters({
+            search: '',
+            page: DEFAULT_PAGE,
+        });
+    };
+    return (
+        <>
+            <NewAgentDialog open={isDialogOpen} onOpenChange={setIsDialogOpen} />
+            <div className="flex flex-col gap-y-4 px-4 py-4 md:px-8">
+                <div className="flex items-center justify-between">
+                    <h5 className="text-xl font-medium">My Agents</h5>
+                    <Button onClick={() => setIsDialogOpen(true)}>
+                        <PlusIcon />
+                        New Agent
+                    </Button>
+                </div>
+                <div className="flex items-center gap-x-2 p-1">
+                    <AgentsSearchFilter />
+                    {isAnygFilterModified && (
+                        <Button variant={'outline'} size={'sm'} onClick={onClearFilters}>
+                            <XCircleIcon />
+                            Clear
+                        </Button>
+                    )}
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default AgentListHeader;
