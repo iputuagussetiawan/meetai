8b477710675040e558c7540a81b06c0fca69f33f
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-06-03 13:52:46 +0800

    agent page and pagination

--- src/modules/agents/ui/components/data-pagination.tsx
+++ src/modules/agents/ui/components/data-pagination.tsx
@@ -1,22 +1,22 @@
-interface Props{
+interface Props {
   page: number;
   totalPages: number;
   onPageChange: (page: number) => void;
 }
 
-import { Button } from '@/components/ui/button';
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
-const DataPagination = ({page, totalPages, onPageChange}: Props) => {
+const DataPagination = ({ page, totalPages, onPageChange }: Props) => {
   return (
-    <div className='flex items-center justify-between'>
-      <div className='flex-1 text-sm text-muted-foreground'>
+    <div className="flex items-center justify-between">
+      <div className="flex-1 text-sm text-muted-foreground">
         Page {page} of {totalPages || 1}
       </div>
-      <div className='flex items-center justify-end space-x-2 py-4'>
-        <Button 
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button
           variant={"outline"}
-          disabled={page==1}
+          disabled={page == 1}
           size={"sm"}
           onClick={() => onPageChange(Math.max(1, page - 1))}
         >
@@ -25,14 +25,14 @@ const DataPagination = ({page, totalPages, onPageChange}: Props) => {
         <Button
           variant={"outline"}
           size={"sm"}
-          disabled={page==totalPages || totalPages===0} 
+          disabled={page == totalPages || totalPages === 0}
           onClick={() => onPageChange(Math.min(totalPages, page + 1))}
         >
           Next
         </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DataPagination
+export default DataPagination;
