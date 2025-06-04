67dc024bc256d97af3baea8330b6ba63e8382509
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-06-04 15:57:09 +0800

    meeting form

---
+++ src/modules/meetings/schema.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const meetingInsertSchema = z.object({
+	name: z.string().min(1, {
+		message: 'Name is required.',
+	}),
+	agentId: z.string().min(1, {
+		message: 'Agent is required.',
+	}),
+});
+
+export const meetingUpdateSchema = meetingInsertSchema.extend({
+	id: z.string().min(1, {
+		message: 'Id is required.',
+	}),
+});
