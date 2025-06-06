08c1d24124af52917e726d5ee6bfa01a21a8c330
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-06-06 11:22:24 +0800

    add modul video call-> but some error on call-lobby.jsx

--- src/modules/call/ui/views/call-view.tsx
+++ src/modules/call/ui/views/call-view.tsx
@@ -1,7 +1,9 @@
 'use client';
+import ErrorState from '@/components/error-state';
 import { useTRPC } from '@/trpc/client';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import React from 'react';
+import CallProvider from '../components/call-provider';
 
 interface Props {
 	meetingId: string;
@@ -9,7 +11,15 @@ interface Props {
 const CallView = ({ meetingId }: Props) => {
 	const trpc = useTRPC();
 	const { data } = useSuspenseQuery(trpc.meetings.getOne.queryOptions({ id: meetingId }));
-	return <div>{JSON.stringify(data, null, 2)}</div>;
+
+	if (data.status === 'completed') {
+		return (
+			<div className="flex h-screen items-center justify-center">
+				<ErrorState title="Meeting has ended" description="Please try again later." />
+			</div>
+		);
+	}
+	return <CallProvider meetingId={meetingId} meetingName={data.name}></CallProvider>;
 };
 
 export default CallView;
